6c4f2ceb4f6172871700a347d8c66651ade77b7e
thaboRach <masipathabo.rachidi@vcontractor.co.za>
2024-10-16 15:18:42 +0200

    feat(actions): deploy to npm

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   prefix: 'ra-',
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
+
+export default config;
